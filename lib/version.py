a70253cc923ee7f871bd5ae92ba0d58634e97ace
root <root@eidolon.deimoscoin.org>
2019-03-14 03:20:19 +0000

    DEI Electrum

---
+++ lib/version.py
@@ -0,0 +1,17 @@
+ELECTRUM_VERSION = '3.2.0'   # version of the client package
+PROTOCOL_VERSION = '1.2'     # protocol version requested
+APK_VERSION = '3.2.0.0'      # read by buildozer.spec
+
+# The hash of the mnemonic seed must begin with this
+SEED_PREFIX      = '01'      # Standard wallet
+SEED_PREFIX_2FA  = '101'     # Two-factor authentication
+SEED_PREFIX_SW   = '100'     # Segwit wallet
+
+
+def seed_prefix(seed_type):
+    if seed_type == 'standard':
+        return SEED_PREFIX
+    elif seed_type == 'segwit':
+        return SEED_PREFIX_SW
+    elif seed_type == '2fa':
+        return SEED_PREFIX_2FA
